fadd469fb2ba4ab294835a3b0a95e6f2aaf4659a
EricJames <ericjamessonio7@gmail.com>
2025-04-22 17:31:51 -0800

    Updated

--- exercise.py
+++ exercise.py
@@ -37,6 +37,27 @@ class Researchpaper(LibraryItem):
     def get_details(self):
         return f"Title : {self.title}, Barcode : {self.barcode}, Author : {self.author}, Topic : {self.topic}"
 
+class Student:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        self.borrowed_items = []
+        
+    def borrow_item(self, library : "Library", barcode ):
+        library.lend_item(self, barcode)
+    
+    def back_item(self, barcode, library : "Library"):
+        for item in self.borrowed_items:
+            if barcode == item.barcode:
+                self.borrowed_items.remove(item)
+                library.add_item(item)
+                print(f" ITem : {item.title} Back to the library")
+                
+    def display_borrowed_items(self):
+        for item in self.borrowed_items:
+            print(item.get_details())
+            
+        
 class Library:
     def __init__(self, name, location, owner):
         self.name = name
@@ -51,31 +72,25 @@ class Library:
         for item in self.items:
             if barcode == item.barcode:  
                 self.items.remove(item) 
-            else:
-                return "Item doesnt exist"     
-
+                return f"Item {item.title} Remove!"
+        return "Item doesnt exist"     
+    
+    def lend_item(self, student : Student, barcode):
+        for item in self.items:
+            if barcode == item.barcode:
+                self.items.remove(item)
+                student.borrowed_items.append(item)
+                print(f"Item : {item.title} is Being Borrowed")
+                return
+        print("Item doesnt exist")
+                
 
     def display(self):
         for item in self.items:
             print(item.get_details())
-        
-
-
-
-
-class Student:
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
-        self.borrowed_items = []
-        
-
-        
+            
 
     
-
-    
-
 if __name__ == "__main__":
     student1 = Student("Eric", 789)
     library = Library("Library Store", "Bagong Bariio", "Eric")
